2f701835604f570f31be81edd3096a53b1edfcea
lvzijian.lzj <lvzijian.lzj@alibaba-inc.com>
2026-02-28 17:04:15 +0800

    add z-image pipeline

--- diffsynth_engine/configs/__init__.py
+++ diffsynth_engine/configs/__init__.py
@@ -1,7 +1,9 @@
 from .base import PipelineConfig
 from .qwen_image import QwenImagePipelineConfig
+from .z_image import ZImagePipelineConfig
 
 __all__ = [
     "PipelineConfig",
     "QwenImagePipelineConfig",
+    "ZImagePipelineConfig",
 ]
